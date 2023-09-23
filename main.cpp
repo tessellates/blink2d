9078a1fa2e5fa84f7a40cfff35e5efcc4fd106db
Tesselates <Tobias.e.Huysentruyt@gmail.com>
2023-09-23 22:34:48 +0200

    clean main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,3 @@
-// Dear ImGui: standalone example application for SDL2 + SDL_Renderer
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
-// Important to understand: SDL_Renderer is an _optional_ component of SDL2.
-// For a multi-platform app consider using e.g. SDL+DirectX on Windows and SDL+OpenGL on Linux/OSX.
-
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
@@ -25,19 +13,10 @@
 int main(int, char**)
 {
     blink2dgui::Application& app = *blink2dgui::Application::GetInstance();
-
     while (app.isRunning())
     {
         app.loop();
     }
     blink2dgui::Application::Destroy(app);
-
-    blink2dgui::Application& app2 = *blink2dgui::Application::GetInstance();
-
-    while (app2.isRunning())
-    {
-        app2.loop();
-    }
-    blink2dgui::Application::Destroy(app2);
     return 0;
 }
